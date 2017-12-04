7c9e9d7696ffe4028c639d6c70250bbed4e30922
Mauro Tortonesi <mauro.tortonesi@unife.it>
2017-12-05 00:00:12 +0100

    Improved (basic) testing.

--- test/fastrandom_test.rb
+++ test/fastrandom_test.rb
@@ -1,13 +1,22 @@
 require "test_helper"
 require "fastrandom/random"
 
-class FastrandomTest < Minitest::Test
+class FastRandomTest < Minitest::Test
+  @@SAMPLE_SIZE = 10_000
+
   def test_that_it_has_a_version_number
     refute_nil ::FastRandom::VERSION
   end
 
-  def test_it_does_something_useful
+  def test_it_does_not_return_nil
     frng = FastRandom::Random.new
-    refute_nil frng.rand
+    samples = Array.new(@@SAMPLE_SIZE) { frng.rand }
+    assert samples.all?
+  end
+
+  def test_samples_between_0_and_1
+    frng = FastRandom::Random.new
+    samples = Array.new(@@SAMPLE_SIZE) { frng.rand }
+    assert samples.all? {|s| 0.0 < s and s < 1.0 }
   end
 end
